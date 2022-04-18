a5b5dbca72b805c661243b4ab0f4e10eef158cd2
tmpltn <tmpltn@a.team>
2022-04-18 08:43:25 -0700

    add dapp, start design + words

--- dapp/src/App.tsx
+++ dapp/src/App.tsx
@@ -1,25 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ThemeProvider } from "@mui/material/styles";
+import { theme } from "./theme";
+
+import { Box, Container, CssBaseline, Link, Stack } from "@mui/material";
+import Docs from "./Docs";
+import Mint from "./Mint";
+import {
+  BrowserRouter,
+  Outlet,
+  Route,
+  Routes,
+  Link as RouterLink,
+} from "react-router-dom";
+import Home from "./Home";
+import NoMatch from "./NoMatch";
+import Gauge from "./Gauge";
+import Nav from "./Nav";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="docs" element={<Docs />} />
+          <Route path="gauge/:id" element={<Gauge />} />
+          <Route path="*" element={<NoMatch />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+function Layout() {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Container maxWidth="md">
+        <Stack spacing={3} sx={{ my: 6 }}>
+          <Header />
+          <Nav />
+          <Outlet />
+        </Stack>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ dapp/src/Header.tsx
@@ -0,0 +1,12 @@
+import { Box, Card, Link, Typography } from "@mui/material";
+import { Link as RouterLink } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <Box>
+      <Typography variant="h1">MERC</Typography>
+    </Box>
+  );
+};
+
+export default Header;
